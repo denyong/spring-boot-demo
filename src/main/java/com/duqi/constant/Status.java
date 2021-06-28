ead42e23f3e526b5ebb3fcc832ca8d05196ebb98
dengyong <13661341683@163.com>
2021-06-28 15:48:43 +0800

    规范

--- src/main/java/com/duqi/constant/Status.java
+++ src/main/java/com/duqi/constant/Status.java
@@ -4,18 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum Status {
-  INSTALLING("installing", "安装中"), // 安装中
-  INSTALL_FAIL("install_fail", "安装失败"), // 安装失败
-  RUNNING("running", "运行中"), // 运行中
-  UNINSTALLING("uninstalling", "卸载中"), // 卸载中
-  UNINSTALL_FAIL("uninstall_fail", "卸载失败"), // 卸载失败
-  UNINSTALLED("uninstalled", "已卸载"); // 已卸载
+    INSTALLING("installing", "安装中"),
+    INSTALL_FAIL("install_fail", "安装失败"),
+    RUNNING("running", "运行中"),
+    UNINSTALLING("uninstalling", "卸载中"),
+    UNINSTALL_FAIL("uninstall_fail", "卸载失败"),
+    UNINSTALLED("uninstalled", "已卸载");
 
-  private String key;
-  private String value;
+    private String key;
+    private String value;
 
-  Status(String key, String value) {
-    this.key = key;
-    this.value = value;
-  }
+    Status(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
 }
